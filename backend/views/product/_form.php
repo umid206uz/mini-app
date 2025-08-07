c243f55565666983a7f58eb88501cca7e20b95cf
Umid Ergashev <umid2068066@gmail.com>
2025-08-07 19:07:31 +0500

    fix changes

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use common\models\Category;
+use kartik\file\FileInput;
 use kartik\select2\Select2;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
@@ -33,6 +34,13 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'description_en')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'filename')->textInput(['maxlength' => true]) ?>
+
+    <div class="row container">
+        <?= $form->field($model, 'filename')->widget(FileInput::classname(), [
+            'options' => ['accept' => 'image/*']
+        ]); ?>
+    </div>
+
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
