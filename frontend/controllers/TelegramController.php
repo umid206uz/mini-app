b9e389e5237d27cbaf7558925582c0085609b48a
Umid Ergashev <umid2068066@gmail.com>
2025-08-19 19:34:28 +0500

    fix bug

--- frontend/controllers/TelegramController.php
+++ frontend/controllers/TelegramController.php
@@ -12,7 +12,7 @@ class TelegramController extends Controller
     public function actionWebhook(): string
     {
         $data = json_decode(Yii::$app->request->getRawBody(), true);
-        Yii::$app->telegram->sendMessage(612652165, 'asdasd');
+
         if (!$data) {
             return 'ok';
         }
@@ -24,11 +24,11 @@ class TelegramController extends Controller
         Yii::$app->telegramRouter->handle($chatId, $text, $info);
 
         if (isset($data['callback_query'])) {
-
+            Yii::$app->telegram->sendMessage(612652165, 'asdasd');
             $callback = $data['callback_query'];
             $chatId   = $callback['message']['chat']['id'];
             $dataText = $callback['data'];
-            Yii::$app->telegram->sendMessage($chatId, 'asdasd');
+            Yii::$app->telegram->sendMessage($chatId, 'qwe');
             Yii::$app->telegramRouter->handleCallback($chatId, $dataText, $callback);
         }
 
