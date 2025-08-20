3526c66533610d9c7892458f374e8b254161dfe4
Umid Ergashev <umid2068066@gmail.com>
2025-08-20 17:34:23 +0500

    fix bugs

--- common/telegram/handlers/OrderCallbackHandler.php
+++ common/telegram/handlers/OrderCallbackHandler.php
@@ -34,6 +34,7 @@ class OrderCallbackHandler
             $tx = $db->beginTransaction();
             try {
                 $cart_items = Cart::find()->where(['user_id' => $chatId, 'status' => Cart::STATUS_ACTIVE])->all();
+                Yii::$app->telegram->sendMessage($chatId, json_encode($cart_items));
                 if (!$cart_items) {
                     Yii::$app->telegram->sendMessage($chatId, TextFactory::emptyCartText());
                     $tx->rollBack();
@@ -51,7 +52,7 @@ class OrderCallbackHandler
                 $total = 0;
                 foreach ($cart_items as $cart_item) {
                     /** @var Cart $cart_item */
-                    Yii::$app->telegram->sendMessage($chatId, 'asdasd');
+
                     $sum = $cart_item->quantity * $cart_item->price;
                     $item = new OrderItems();
                     $item->order_id = $order->id;
