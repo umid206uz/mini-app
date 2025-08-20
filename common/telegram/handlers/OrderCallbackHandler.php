5279c03062033b383da5c27615276dd33c1df6d4
Umid Ergashev <umid2068066@gmail.com>
2025-08-20 17:58:28 +0500

    fix bugs

--- common/telegram/handlers/OrderCallbackHandler.php
+++ common/telegram/handlers/OrderCallbackHandler.php
@@ -31,9 +31,6 @@ class OrderCallbackHandler
 
         if ($text_button == 'order_confirm') {
             $cartItems = Cart::find()->where(['user_id' => $chatId, 'status' => Cart::STATUS_ACTIVE])->all();
-            foreach ($cartItems as $asd){
-                Yii::$app->telegram->sendMessage($chatId, $asd->created_at);
-            }
 
             if (!$cartItems) {
                 Yii::$app->telegram->sendMessage($chatId, TextFactory::emptyCartText());
@@ -51,7 +48,7 @@ class OrderCallbackHandler
             $total = 0;
             foreach ($cartItems as $cartItem) {
                 /** @var Cart $cartItem */
-
+                Yii::$app->telegram->sendMessage($chatId, $cartItem->created_at);
                 $sum = $cartItem->quantity * $cartItem->price;
                 $item = new OrderItems();
                 $item->order_id = $order->id;
@@ -60,19 +57,9 @@ class OrderCallbackHandler
                 $item->price = $cartItem->price;
                 $item->quantity = $cartItem->quantity;
                 $item->total_price = $sum;
-                $item->save();
-                if ($cartItem->hasErrors()){
-                    Yii::$app->telegram->sendMessage($chatId, json_encode($cartItem->getErrors()));
-                }
+                $cartItem->status = Cart::STATUS_INACTIVE;
                 if ($item->save()){
-                    $cartItem->status = Cart::STATUS_INACTIVE;
-                    $cartItem->save();
-                    if ($cartItem->hasErrors()){
-                        Yii::$app->telegram->sendMessage($chatId, json_encode($cartItem->getErrors()));
-                    }
-                    if ($item->hasErrors()){
-                        Yii::$app->telegram->sendMessage($chatId, json_encode($item->getErrors()));
-                    }
+                    Yii::$app->telegram->sendMessage($chatId, 'order items saqlandi');
                 }
 
                 $total += $sum;
