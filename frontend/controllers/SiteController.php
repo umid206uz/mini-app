a174c5d2413230fc18d9f45ca66fc06160b600af
Umid Ergashev <umid2068066@gmail.com>
2025-08-20 17:51:59 +0500

    fix bugs

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -104,6 +104,7 @@ class SiteController extends Controller
 
     public function actionCart(): string
     {
+        dd($cartItems = Cart::find()->where(['user_id' => 612652165, 'status' => Cart::STATUS_ACTIVE])->all());
         $user_id = Yii::$app->session->get('user_id');
         $cart = Cart::find()->where(['user_id' => $user_id, 'status' => Cart::STATUS_ACTIVE])->all();
         return $this->render('cart', [
