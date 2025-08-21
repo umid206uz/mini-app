d3c49e2d2590d69819babded5a3197239d36e529
Umid Ergashev <umid2068066@gmail.com>
2025-08-21 17:59:40 +0500

    fix bugs

--- common/telegram/keyboards/KeyboardFactory.php
+++ common/telegram/keyboards/KeyboardFactory.php
@@ -45,13 +45,7 @@ class KeyboardFactory
                         ]
                     ]
                 ]
-            ],
-            'keyboard' => [
-                [
-                    ['text' => '📋 Buyurtma berish'],
-                    ['text' => '🛒 Savatcha']
-                ],
-            ],
+            ]
         ];
     }
 
